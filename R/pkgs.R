5054f77ae44473c39d2d2fc349e7caa84c34de60
avaghefi <staghizadehvaghefi@kpmg.com>
2017-07-12 16:44:26 -0600

    install the packages if required

---
+++ R/pkgs.R
@@ -0,0 +1,28 @@
+#' install the required packages if they are not available
+#' 
+#' 
+
+package_list <- c('readr'
+                  ,'dplyr'
+                  ,'xts'
+                  ,'zoo'
+                  ,'TTR'
+                  ,'quantmod')
+
+pkg_load <- function(pkg_list){
+  available_packages <- installed.packages()[, "Package"]
+  new_pkgs <- pkg_list[!(pkg_list %in% available_packages)]
+  if (length(new_pkgs)){
+    install.packages(new_pkgs, 
+                     dependencies = TRUE, 
+                     quiet = TRUE) 
+  } 
+  print('I am here')
+  sapply(package_list, 
+         require, 
+         character.only = TRUE, 
+         quietly = TRUE)
+}
+
+pkg_load(package_list)
+
