f9586f14733b1acba3d029d623e0c50316c3cfdc
avaghefi <staghizadehvaghefi@kpmg.com>
2017-07-12 17:01:38 -0600

    convert it to a function

--- R/getthat.R
+++ R/getthat.R
@@ -5,33 +5,31 @@ dir <- "C:/Users/staghizadehvaghefi/Desktop/Learning/PM/R_1/PortfoLio"
 setwd(dir)
 
 #######################################
-# Symbol names:
-
 # Method 1
-# stock_mkt <- c('nasdaq','nyse', 'nysemk')
-# stock_mkt <- c('nasdaq')
-# symb_df <- vector()
-# for (mkt in stock_mkt){
-#   file_name <- paste0(dir, '/','data/', mkt,'.csv')
-#   symb_df <- rbind(symb_df, readr::read_csv(file_name))
-# }
-# method 2
-# stock_mkt <- c("AMEX", "NASDAQ", "NYSE")
-stock_mkt <- c("NASDAQ")
+getthat <- function(symbols
+                    ,length = 60){
+  stock_env <- new.env() #env class
+  stock_obj <- quantmod::getSymbols(symbols 
+                                    ,env = stock_env 
+                                    ,warnings = FALSE,
+                                    from = Sys.Date() - length
+  )
+  data <- list(price_list = stock_env
+               ,daily_return = eapply(stock_env, 
+                                      quantmod::dailyReturn))
+  return(data)
+}
+
+# example
+stock_mkt <- c("AMEX", "NASDAQ", "NYSE")
+# stock_mkt <- c("NASDAQ")
 symb_df <- TTR::stockSymbols(exchange = stock_mkt)
 symb <- symb_df$Symbol
 # sample or all
 symb <- c('AMD', 'MSFT', 'APPN', 'CAMT', 'NVDA', 'AAPL', 'SPI', 'UAL', 'LUV', 'UAL', 'TSLA',
-                 'NFLX', 'FB', 'BABA' )
+          'NFLX', 'FB', 'BABA' )
 
-length_study <- 60
-stock_env <- new.env() #env class
+stock_obj <- getthat(symb,length = 60 )
 
-stock_obj <- quantmod::getSymbols(symb 
-                                 ,env = stock_env 
-                                 ,warnings = FALSE,
-                                 from = Sys.Date() - length_study
-                                 )
-Daily_returns <- eapply(stock_env, 
-                        quantmod::dailyReturn)
+
 
